f25a10a770f6efdb9f22d77744fba45adde5489c
Danilo dos Santos <danilo.santossouza@outlook.com>
2023-05-29 19:13:09 -0300

    configurando o document

---
+++ src/pages/product/index.tsx
@@ -0,0 +1,7 @@
+import { useRouter } from "next/router"
+
+export default function Product() {
+  return (
+    <h1>Product: </h1>
+  )
+}
---
+++ src/pages/success.tsx
@@ -0,0 +1,6 @@
+import Head from 'next/head'
+export default function Success() {
+  return (
+    <h1>Success</h1>
+  )
+}
