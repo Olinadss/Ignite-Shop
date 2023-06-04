9ad29a8a3d722b39df9632ec23a8583fff810c94
Danilo dos Santos <danilo.santossouza@outlook.com>
2023-06-04 10:57:22 -0300

    feature: configura o global css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
-import Head from 'next/head'
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$green300'
+})
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Button>Enviar</Button>
   )
 }
